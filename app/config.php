98d44efef16539e2da58a820674f90b4fc8a070c
LN-T <LN-T@hotmail.com>
2017-12-03 18:13:20 +0100

    Form with Captcha+Swiftmailer template new file:   src/Controllers/ContactController.php => captcha + swiftmailer new file:   src/Views/user/contact/contact.html.twig => add captcha and remove required new file:   src/Views/user/contact/contactMailAdmin.html.twig => template twig w\ css new file:   src/Views/user/contact/contactMailUser.html.twig => template twig w\ css new file:   src/Views/user/contact/contactSuccess.html.twig => rename and move file
    deleted:    Iconr
    deleted:    public/assets/img/Volup't.png
    deleted:    public/assets/img/logo.png
    deleted:    src/Views/user/contact.html.twig
    
    modified:   README.md
    modified:   app/config.php => swiftmailer and captcha passwords
    modified:   composer.json => add Recaptcha
    modified:   composer.lock
    modified:   public/index.php => add ContactController for form + swiftmailer + captcha
    modified:   src/Controllers/ContactController.php => new file
    modified:   src/Controllers/DefaultController.php => move contact actions to ContactController
    modified:   src/Views/admin/adminLayout.html.twig => add gmail link
    modified:   src/Views/layout.html.twig => add admin link in footer

--- app/config.php
+++ app/config.php
@@ -14,4 +14,20 @@ define("APP_DB_PWD", "7gErwRTR6f");
 define("APP_DB_NAME", "sql11206893");
 define("APP_DB_PORT", "3306");
 
+/**
+ * Connect to Swiftmailer
+ * @param [string] APP_CONTACT_MAIL
+ * @param [string] APP_CONTACT_PWD
+ */
+define("APP_CONTACT_MAIL", "contact.volupt@gmail.com");
+define("APP_CONTACT_PWD", "jecode4wcs");
+
+/**
+ * Connect to Recaptcha
+ * @param [string] APP_CAPTCHA_SITEKEY
+ * @param [string] APP_CAPTCHA_SECRET
+ */
+define("APP_CAPTCHA_SITEKEY", '6LeIIzsUAAAAAOUQ2F0nz-vWIJSGl7SwSSGglgRa');
+define("APP_CAPTCHA_SECRET", '6LeIIzsUAAAAAPaLaQiBs9FnEgSOAa9ji7ihNhrL');
+
 ?>
